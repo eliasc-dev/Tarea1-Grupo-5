49352fdf42c5c1b386dd553c073130181ac28b83
eliasc-dev <e.carrenoarriagada@gmail.com>
2025-05-12 16:54:15 -0400

    Funciones

--- Tarea1 Grupo5.py
+++ Tarea1 Grupo5.py
@@ -1,1 +1,8 @@
-hola = 1
+def interfaz_menu():
+    print("""SISTEMA DE PLANIFICACIÓN DE IMPRESIÓN DE REVISTAS ESPECIALIZADAS
+1. Generar plan de impresión
+2. Ingresar cantidad de impresiones por revista especializada y día
+3. Visualizar el ingreso total de una revista especializada específica
+4. Visualizar el ingreso total de todas las revistas especializadas en un día específico
+5. Salir del programa""")
+interfaz_menu()
