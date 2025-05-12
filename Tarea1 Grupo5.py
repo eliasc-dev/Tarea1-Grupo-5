16511b40d8f7abfcd9e414064be82b3a56d91179
eliasc-dev <e.carrenoarriagada@gmail.com>
2025-05-12 16:57:19 -0400

    Opciones

--- Tarea1 Grupo5.py
+++ Tarea1 Grupo5.py
@@ -5,4 +5,33 @@ def interfaz_menu():
 3. Visualizar el ingreso total de una revista especializada específica
 4. Visualizar el ingreso total de todas las revistas especializadas en un día específico
 5. Salir del programa""")
-interfaz_menu()
+
+# Interfaz del menu solicitado, trabajaremos todas las opciones con funciones que seran llamadas con  la variable "opcion".
+
+while True:
+    opcion = int(input("Seleccione su opción: "))
+
+    if opcion == 1:
+        # funcion1() PENDIENTE CREAR 
+        pass
+    
+    elif opcion == 2:
+        #funcion2() PENDIENTE CREAR
+        pass
+    
+    elif opcion == 3:
+        #funcion3() PENDIENTE CREAR
+        pass
+    
+    elif opcion == 4:
+        #funcion4() PENDIENTE CREAR
+        pass
+    
+    elif opcion == 5:
+        break
+    
+    else:
+        print("La Opcion es Invalida, intentelo denuevo.")
+
+    
+
